bd2cb5dd7bf569757616a2f8915144d1ec8c963b
preciousken <pkehinde27@gmail.com>
2023-05-13 22:49:41 +0100

    completed the uploadImages to cloudinary

--- index.js
+++ index.js
@@ -1,50 +1,28 @@
 const express = require('express');
 const cors = require('cors')
-const upload = require('./multer')
-const fs = require('fs')
-const cloudinary = require('./cloudinary')
-
 const app = express();
-
 app.use(cors())
 app.use(express.json())
 
-app.use("/upload-images", upload.array("image") , async(req,res)=>{                                                                 // upload.array("name of image from frontend")
 
-    console.log('second');
 
-    const uploader = async (path) => await cloudinary.uploads(path,"pcare")                                                         // (path, "name of folder in cloudinary")
+const upload = require('./multer')
+const { uploadImage } = require('./uploaderFunction');
 
-    if(req.method === "POST"){
-        const urls = []
-        const files = req.files
 
-        console.log(files);
-        
-        for(const file of files){
-            const {path} = file
-            const newPath = await uploader(path)
-            urls.push(newPath)
-            fs.unlinkSync(path)
-        }
 
-        res.status(200).json({
-            message:"Images Uploaded successfully",
-            data: urls
-        })
-    }else{
-        res.status(405).json({
-            err:'Images not uploaded successfully'
-        })
-    }
+app.post('/upload', upload.array("pcare"), async (req, res) => {
+
+    const urls = await uploadImage(req.files)
+    // write your code that makes use of the images
+
 })
 
-app.get('/',async(req,res)=>{
-    res.send('hello word')
-})
+
+
+
 
 const port = process.env.PORT || 5000
-
-app.listen(port,()=>{
-    console.log('app listening on port '+port);
+app.listen(port, () => {
+    console.log('app listening on port ' + port);
 })
---
+++ uploaderFunction.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const cloudinary = require('./cloudinary')
+
+const uploadImage = async ( filesToUpload ) => {
+
+    const uploader = async (path) => await cloudinary.uploads(path, "pcare")
+
+        const urls = []
+        const files = filesToUpload
+
+        for (const file of files) {
+            const { path } = file
+            const newPath = await uploader(path)
+            urls.push(newPath)
+            fs.unlinkSync(path)
+        }
+
+        return urls
+}
+
+
+module.exports = {uploadImage}
