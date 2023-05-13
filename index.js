536acc335859fc2407f6a7cca9411bb28917b029
preciousken <pkehinde27@gmail.com>
2023-05-13 20:19:30 +0100

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// AudioListener
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/',async(req,res)=>{
+    res.send('hello word')
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port,()=>{
+    console.log('app listening on port'+port);
+})
